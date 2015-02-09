044af727388671d4d3ac87b7eb9aa18115589190
Katie Hoffman <ktahoffman@gmail.com>
2015-02-09 17:43:52 -0500

    add passing code

--- lib/whale.rb
+++ lib/whale.rb
@@ -1,1 +1,37 @@
-# your code here
+class Whale
+
+  attr_accessor :calves, :lbs_of_milk, :lbs_of_krill
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @calves = []
+    @lbs_of_milk = 120
+    @lbs_of_krill = 0
+  end
+
+  def hungry?
+    lbs_of_krill < 2000
+  end
+
+  def feed_calf(calf)
+    if self.lbs_of_milk > 50 && calf.hungry?
+      self.lbs_of_milk -= 50
+      calf.lbs_of_milk += 50
+    elsif calf.hungry?
+      "not enough milk"
+    else
+      "calf isn't hungry"
+    end
+  end
+
+  def give_birth
+    self.calves << Calf.new(self)
+  end
+
+  def adopt_calf(calf)
+    calf.mother = self
+    self.calves << calf
+  end
+  
+end
